91c08553dc718ad7976f041878a47456aa11c6d6
Harrison <hjmyles1@gmail.com>
2018-09-11 15:10:22 -0400

    finished hw0906

--- hw0906/src/Main.java
+++ hw0906/src/Main.java
@@ -1,7 +1,9 @@
 /*
 Myles
 COSC 311 Homework 9/6/2018
-URL:
+URL:             https://github.com/hmyles/COSC311
+URL for project: https://github.com/hmyles/COSC311/tree/master/hw0906
+URL for file:    https://github.com/hmyles/COSC311/tree/master/hw0906/src
 
 
 
@@ -16,7 +18,6 @@ public class Main {
             return new int[0];
 
         int[] retArr = new int[a.length * factor];
-        int arrSize = a.length;
 
         for(int i = 0; i < retArr.length;i++)
             retArr[i] = a[i%a.length];
@@ -28,15 +29,45 @@ public class Main {
 
         for(int i = 0; i < array.length; i++)
             System.out.print(" " + array[i] + " ");
+    }
+
+    public static void report(int[] a, int[] b, int factor){
+
+        System.out.print("a = {");
+        printArray(a);
+        System.out.println("}");
+
+        System.out.println("factor = " + factor);
         System.out.println();
+
+        System.out.print("repeat(a, factor) --> {");
+        printArray(b);
+        System.out.println("}");
+        System.out.println();
+
     }
 
     public static void main(String[] args){
 
-        int[] a = {1, 2, 3, 4};
-        int[] b = repeat(a,2);
 
-        printArray(b);
+        int[] a = {1, 2, 3};
+        int factor = 1;
+        int[] b = repeat(a, factor);
+        report(a, b, factor);
+
+
+        factor = 2;
+        b = repeat(a, factor);
+        report(a, b, factor);
+
+
+
+        factor = -2;
+        b = repeat(a, factor);
+        report(a, b, factor);
+
+
+
 
     }
 
