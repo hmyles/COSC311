99e35b20879cb0930ddf8c9272f058b372e910cc
Harrison <hjmyles1@gmail.com>
2018-09-16 19:41:31 -0400

    added basic functions to Node and LinkedList

--- hw0911/src/LinkedList.java
+++ hw0911/src/LinkedList.java
@@ -5,9 +5,12 @@ public class LinkedList {
     public LinkedList(double[] arr){
 
         for(double d : arr){
-            
+
         }
 
     }
 
+    public void insert(double num){}
+    public boolean delete(double num){}
+    public String toString(){}
 }
--- hw0911/src/Node.java
+++ hw0911/src/Node.java
@@ -1,2 +1,16 @@
 public class Node {
+
+    private Node next;
+    private double data;
+
+    public Node(double num){
+
+        data = num;
+
+    }
+
+    public Node next(){}
+    public void setNext(){}
+    public String toString(){}
+
 }
