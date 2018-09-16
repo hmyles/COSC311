27b7e3d930d89ada805d9fd8dbc3ebdf31a4b86d
Harrison <hjmyles1@gmail.com>
2018-09-16 19:15:17 -0400

    init hw0911

---
+++ hw0911/src/Main.java
@@ -0,0 +1,24 @@
+/*
+
+Myles
+hw0911
+url:
+
+ */
+
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+        double[] input1, input2, input3;
+
+        input1 = new double[]{100.0};
+
+
+
+
+    }
+
+}
