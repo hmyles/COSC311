b1d944eb497e28f10b037a055fa59a5192b6f89f
Harrison <hjmyles1@gmail.com>
2018-10-04 13:00:02 -0400

    Made queue for new hw, writing main now

---
+++ hw0927/src/CQueue.java
@@ -0,0 +1,60 @@
+public class CQueue {
+
+    private int[] data;
+    private int current_size, max_size, head, tail;
+
+
+    public CQueue(int size){
+
+        data = new int[size];
+        max_size = size;
+        current_size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    public boolean isFull(){
+        return (current_size + 1) == max_size;
+    }
+
+    public boolean isEmpty(){
+        return current_size == 0;
+    }
+
+    public boolean push(int x){
+
+        if(isFull())
+            return false;
+
+        data[tail] = x;
+        tail++;
+        current_size++;
+
+        tail = tail % max_size;
+
+        return true;
+    }
+
+    public boolean pop(){
+
+        if(isEmpty())
+            return false;
+
+        head++;
+        head = head % max_size;
+        current_size--;
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        String display = "";
+        int  h_temp = head;
+
+        for(int i = 0; i < current_size; i++)
+            display += data[h_temp++%max_size] + ", ";
+
+        return display;
+    }
+}
--- hw0927/src/Main.java
+++ hw0927/src/Main.java
@@ -1,4 +1,21 @@
+/*
+Myles
+HW 0927
+URL: https://github.com/hmyles/COSC311/tree/master/hw0927/src
+
+ */
+
 public class Main {
 
-    
+    public static CQueue getQueue(int n){
+
+        return new CQueue(n);
+    }
+
+
+    public static void main(String[] args){
+        
+
+
+    }
 }
